1d0f1e2b538448a8c5b99e2c33b47c05db86a9a6
starpacker <3067025832@qq.com>
2026-04-01 13:16:23 +0800

    feat: complete environment suite with anti-cheating validation

--- atlas/environments/__init__.py
+++ atlas/environments/__init__.py
@@ -1,1 +1,18 @@
-"""Experiment environments with anonymized knob/detector interfaces."""
+"""Experiment environments with anonymized knob/detector interfaces.
+
+Import all environment modules to trigger registration.
+"""
+from atlas.environments import (  # noqa: F401
+    env_01_photoelectric,
+    env_02_compton,
+    env_03_electron_diffraction,
+    env_04_double_slit,
+    env_05_blackbody,
+    env_06_hydrogen_spectrum,
+    env_07_stern_gerlach,
+    env_08_water_wave,
+    env_09_elastic_collision,
+    env_10_spring,
+    env_11_freefall,
+    env_12_heat_conduction,
+)
